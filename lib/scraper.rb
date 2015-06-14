1bd90b10fdb8d34f15796f742fe021ea3b9c09f2
Mark Silverberg <mark.silverberg@socrata.com>
2015-06-14 14:38:03 -0400

    added scraping for sections 1 through 18 which include compliance to specific codes

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -103,10 +103,10 @@ module Inspectr
     def get_form_data(read_file, write_file)
       form_array = self.file_to_array(read_file)
       CSV.open(write_file, "wb") do |csv|
-        csv << ["business_id","name","address","city","state","postal_code","date","score","grade"]
+        csv << ["business_id","name","address","city","state","postal_code","date","score","grade","1-2","2-1A","2-1B","2-1C","2-2A","2-2B","2-2C","2-2D","3-1A","3-1B","3-1C","4-1A","4-1B","4-2A","4-2B","5-1A","5-1B","5-2","6-1A","6-1B","6-1C","6-1D","6-2","7-1","8-2A","8-2B","9-2","10A","10B","10C","10D","11A","11B","11C","11D","12A","12B","12C","12D","13A","13B","14A","14B","14C","14D","15A","15B","15C","16A","16B","16C","17A","17B","17C","17D","18"]
         form_array.each_with_index do |form_link, index|
-        # begin
-        #   Retriable.retriable on: OpenURI::HTTPError, tries: 5, base_interval: 1.5 do
+        begin
+          Retriable.retriable on: OpenURI::HTTPError, tries: 5, base_interval: 1.5 do
             doc = Nokogiri::HTML(open(form_link))
             puts "importing data: #{index + 1} out of #{form_array.length}..."
 
@@ -124,32 +124,43 @@ module Inspectr
             permit = self.restaurant_info(doc,"Permit #").strip
 
             zipcode = self.restaurant_info(doc,"Zipcode").strip
-            # current_grade = self.restaurant_score("#div_grade",doc)
+
             current_score = self.restaurant_score("#div_finalScore",doc).to_i
 
             current_grade = self.restaurant_score("#div_grade",doc).strip
 
-            compliance_columns = ["","in compliance","not incompliance","not applicable","not observed","","corrected on-site during inspection","repeat violation of the same code provision"]
+            # compliance data
+            comp_data = Hash.new
+            compliance_columns_1_to_9 = ["","in compliance","not in compliance","not applicable","not observed","","corrected on-site during inspection","repeat violation of the same code provision"]
+            compliance_columns_10_to_18 = ["","not in compliance","","corrected on-site during inspection","repeat violation of the same code provision"]
+
             doc.css("img[src=\"../../images/circle_closed2.jpg\"]").each do |filled_circle|
-              items_hash = {}
               row = filled_circle.parent.parent
-              items_hash[:row_desc] = row.css("td:nth(6)").text.to_s.strip
-              
+              row_desc = row.css("td:nth(6)").text.to_s.strip # GA form sections 1-9
+              row_desc = row.css("td:nth(3)").text.to_s.strip if row_desc == "" # GA form sections 10-18
+              next if row_desc == "" 
+
+              row_id = row_desc.scan(/(.*)\. /)[0][0]
+              row_id_n = row_id.scan(/^(\d+)-?/)[0][0].to_i
+
               filled_columns = []
               row.css("td").each_with_index do |col,i|
-                filled_columns << compliance_columns[i] if col.to_s.match('circle_closed2.jpg')
+                if row_id_n <= 9
+                  filled_columns << compliance_columns_1_to_9[i] if col.to_s.match('circle_closed2.jpg')
+                elsif row_id_n >= 10
+                  filled_columns << compliance_columns_10_to_18[i] if col.to_s.match('circle_closed2.jpg')
+                end
               end
-              items_hash[:result] = filled_columns.join(" - ")
-              puts items_hash
-
+              # actually set the value for the compliance row 
+              comp_data[row_id] = filled_columns.join(" - ")
             end
-            exit
-            csv << [permit, restaurant_name,street,city,state,zipcode,inspection_date,current_score,current_grade]
-        #   end
-        # rescue => e
-        #   # run this if retriable ends up re-rasing the exception
-        #   puts "!!! we were unable to get data from #{form_link}"
-        # end
+            # add inspection report's data to CSV
+            csv << [permit, restaurant_name, street, city, state, zipcode, inspection_date, current_score, current_grade, comp_data["1-2"],comp_data["2-1A"],comp_data["2-1B"],comp_data["2-1C"],comp_data["2-2A"],comp_data["2-2B"],comp_data["2-2C"],comp_data["2-2D"],comp_data["3-1A"],comp_data["3-1B"],comp_data["3-1C"],comp_data["4-1A"],comp_data["4-1B"],comp_data["4-2A"],comp_data["4-2B"],comp_data["5-1A"],comp_data["5-1B"],comp_data["5-2"],comp_data["6-1A"],comp_data["6-1B"],comp_data["6-1C"],comp_data["6-1D"],comp_data["6-2"],comp_data["7-1"],comp_data["8-2A"],comp_data["8-2B"],comp_data["9-2"],comp_data["10A"],comp_data["10B"],comp_data["10C"],comp_data["10D"],comp_data["11A"],comp_data["11B"],comp_data["11C"],comp_data["11D"],comp_data["12A"],comp_data["12B"],comp_data["12C"],comp_data["12D"],comp_data["13A"],comp_data["13B"],comp_data["14A"],comp_data["14B"],comp_data["14C"],comp_data["14D"],comp_data["15A"],comp_data["15B"],comp_data["15C"],comp_data["16A"],comp_data["16B"],comp_data["16C"],comp_data["17A"],comp_data["17B"],comp_data["17C"],comp_data["17D"],comp_data["18"]]
+          end
+        rescue => e
+          # run this if retriable ends up re-rasing the exception
+          puts "!!! we were unable to get data from #{form_link}"
+        end
         end
       end
     end
--- run.rb
+++ run.rb
@@ -6,10 +6,10 @@ folder_for_session = "data/#{scraping_key}"
 FileUtils.mkdir_p(folder_for_session)
 
 # 1) get inspection pages
-# page_scraper = Inspectr::PageScraper.new("http://ga.healthinspections.us/georgia/","01/01/2015","06/01/2015")
-# page_scraper.all_inspections("#{folder_for_session}/inspection_links.txt",0,0) # pages to get links for
+page_scraper = Inspectr::PageScraper.new("http://ga.healthinspections.us/georgia/","01/01/2015","06/01/2015")
+page_scraper.all_inspections("#{folder_for_session}/inspection_links.txt",0,5) # pages to get links for
 
 # 2) get actual inspection data
 form_scraper = Inspectr::FormScraper.new("#{folder_for_session}/inspection_links.txt","http://ga.healthinspections.us/")
-# form_scraper.get_form_links("#{folder_for_session}/form_links.txt")
+form_scraper.get_form_links("#{folder_for_session}/form_links.txt")
 form_scraper.get_form_data("#{folder_for_session}/form_links.txt","#{folder_for_session}/data.csv")
